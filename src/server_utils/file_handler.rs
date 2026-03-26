97de66ee74c106adc0f96d20539d91298c87c211
Giorbertopazzosgravato <giaggioconsani@gmail.com>
2026-03-26 22:56:22 +0100

    todo: fare la thread pool

--- src/server_utils/file_handler.rs
+++ src/server_utils/file_handler.rs
@@ -1,6 +1,27 @@
-use std::path::PathBuf;
+use std::fs;
+use std::path::Path;
 
-pub struct FileHandler{
-    base_path: PathBuf,
-    
+const DIST: &str = "dist/";
+const FOTO: &str = "foto/";
+
+pub struct FileHandler;
+impl FileHandler{
+    pub fn get_file<P: AsRef<Path> + std::fmt::Debug>(path: P) -> Result<Vec<u8>, Vec<u8>>{
+        if let Some(path) = is_path_safe(path){
+            match fs::File::open(&path){
+                Ok(mut file_content) => {
+                    let content_type = Self::get_content_type(path.extension());
+                    let mut buffer = vec![];
+                    let bytes_read = file_content.read_to_end(&mut buffer).unwrap_or(0);
+                    if bytes_read > 0{ Ok((buffer, content_type)) } else{ println!("fuck"); Err("fuck".as_bytes().to_owned()) }
+                }
+                Err(_) => { Err(Self::get_error_page()) }
+            }
+        } else{
+            get_404_page()
+        }
+
+
+        Ok("ciao".to_string().into_bytes())
+    }
 }
